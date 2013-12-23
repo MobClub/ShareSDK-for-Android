57bce7b404fb791dc8231190611ca4cade1b7504
Alex Yu <alexyu.yxj@gmail.com>
2013-12-23 18:39:13 +0800

    提交2.2.0版本
    1、添加易信分享文字、图片、音乐、视频和网页
    2、添加易信朋友圈分享文字、图片、音乐、视频和网页
    3、添加向微信收藏发送文字、图片、音乐、视频、网页和文件
    4、QZone分享使用新的接口
    5、快捷分享屏蔽QZone一键分享功能
    6、快捷分享添加对微信分享动画的支持
    7、新增插件服务框架
    8、修复快捷分享instagram不能使用的问题

---
+++ Sample/src/cn/sharesdk/demo/AuthPage.java
@@ -0,0 +1,278 @@
+/*
+ * 官网地站:http://www.ShareSDK.cn
+ * 技术支持QQ: 4006852216
+ * 官方微信:ShareSDK   （如果发布新版本的话，我们将会第一时间通过微信将版本更新内容推送给您。如果使用过程中有任何问题，也可以通过微信与我们取得联系，我们将会在24小时内给予回复）
+ *
+ * Copyright (c) 2013年 ShareSDK.cn. All rights reserved.
+ */
+
+package cn.sharesdk.demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import m.framework.ui.widget.slidingmenu.SlidingMenu;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.framework.TitleLayout;
+import cn.sharesdk.onekeyshare.ShareCore;
+
+/**
+ * 授权和取消授权演示页面
+ * <p>
+ * 由于UI显示的需要授权过的平台显示账户的名称，
+ *因此此页面事实上展示的是“获取用户资料”和“取消
+ *授权”两个功能。如果想看纯粹的“授权”操作，请参
+ *考{@link GetTokenPage}页面的相关代码。
+ */
+public class AuthPage extends SlidingMenuPage implements
+		OnClickListener, PlatformActionListener {
+	private View pageView;
+	private TitleLayout llTitle;
+	private AuthAdapter adapter;
+
+	public AuthPage(SlidingMenu menu) {
+		super(menu);
+		pageView = getPage();
+
+		llTitle = (TitleLayout) pageView.findViewById(R.id.llTitle);
+		llTitle.getBtnBack().setOnClickListener(this);
+		llTitle.getTvTitle().setText(R.string.sm_item_auth);
+
+		ListView lvPlats = (ListView) pageView.findViewById(R.id.lvPlats);
+		lvPlats.setSelector(new ColorDrawable());
+		adapter = new AuthAdapter(this);
+		lvPlats.setAdapter(adapter);
+		lvPlats.setOnItemClickListener(adapter);
+	}
+
+	protected View initPage() {
+		return LayoutInflater.from(menu.getContext())
+				.inflate(R.layout.page_auth, null);
+	}
+
+	/** 授权和取消授权的逻辑代码 */
+	public void onClick(View v) {
+		if (v.equals(llTitle.getBtnBack())) {
+			if (menu.isMenuShown()) {
+				menu.hideMenu();
+			}
+			else {
+				menu.showMenu();
+			}
+		}
+	}
+
+	public void onComplete(Platform plat, int action,
+			HashMap<String, Object> res) {
+		Message msg = new Message();
+		msg.arg1 = 1;
+		msg.arg2 = action;
+		msg.obj = plat;
+		handler.sendMessage(msg);
+	}
+
+	public void onError(Platform plat, int action, Throwable t) {
+		t.printStackTrace();
+
+		Message msg = new Message();
+		msg.arg1 = 2;
+		msg.arg2 = action;
+		msg.obj = plat;
+		handler.sendMessage(msg);
+	}
+
+	public void onCancel(Platform plat, int action) {
+		Message msg = new Message();
+		msg.arg1 = 3;
+		msg.arg2 = action;
+		msg.obj = plat;
+		handler.sendMessage(msg);
+	}
+
+	/**
+	 * 处理操作结果
+	 * <p>
+	 * 如果获取到用户的名称，则显示名称；否则如果已经授权，则显示
+	 *平台名称
+	 */
+	public boolean handleMessage(Message msg) {
+		Platform plat = (Platform) msg.obj;
+		String text = MainActivity.actionToString(msg.arg2);
+		switch (msg.arg1) {
+			case 1: {
+				// 成功
+				text = plat.getName() + " completed at " + text;
+				Toast.makeText(menu.getContext(), text, Toast.LENGTH_SHORT).show();
+			}
+			break;
+			case 2: {
+				// 失败
+				text = plat.getName() + " caught error at " + text;
+				Toast.makeText(menu.getContext(), text, Toast.LENGTH_SHORT).show();
+				return false;
+			}
+			case 3: {
+				// 取消
+				text = plat.getName() + " canceled at " + text;
+				Toast.makeText(menu.getContext(), text, Toast.LENGTH_SHORT).show();
+				return false;
+			}
+		}
+
+		adapter.notifyDataSetChanged();
+		return false;
+	}
+
+	private static class AuthAdapter extends BaseAdapter implements OnItemClickListener {
+		private AuthPage page;
+		private ArrayList<Platform> platforms;
+
+		public AuthAdapter(AuthPage page) {
+			this.page = page;
+
+			// 获取平台列表
+			Platform[] tmp = ShareSDK.getPlatformList(page.menu.getContext());
+			platforms = new ArrayList<Platform>();
+			for (Platform p : tmp) {
+				String name = p.getName();
+				if (!ShareCore.canAuthorize(p.getContext(), name)) {
+					continue;
+				}
+				platforms.add(p);
+			}
+		}
+
+		public int getCount() {
+			return platforms == null ? 0 : platforms.size();
+		}
+
+		public Platform getItem(int position) {
+			return platforms.get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = View.inflate(page.menu.getContext(), R.layout.auth_page_item, null);
+			}
+
+			int count = getCount();
+			View llItem = convertView.findViewById(R.id.llItem);
+			int dp_10 = cn.sharesdk.framework.utils.R.dipToPx(parent.getContext(), 10);
+			if (count == 1) {
+				llItem.setBackgroundResource(R.drawable.list_item_single_normal);
+				llItem.setPadding(0, 0, 0, 0);
+				convertView.setPadding(dp_10, dp_10, dp_10, dp_10);
+			}
+			else if (position == 0) {
+				llItem.setBackgroundResource(R.drawable.list_item_first_normal);
+				llItem.setPadding(0, 0, 0, 0);
+				convertView.setPadding(dp_10, dp_10, dp_10, 0);
+			}
+			else if (position == count - 1) {
+				llItem.setBackgroundResource(R.drawable.list_item_last_normal);
+				llItem.setPadding(0, 0, 0, 0);
+				convertView.setPadding(dp_10, 0, dp_10, dp_10);
+			}
+			else {
+				llItem.setBackgroundResource(R.drawable.list_item_middle_normal);
+				llItem.setPadding(0, 0, 0, 0);
+				convertView.setPadding(dp_10, 0, dp_10, 0);
+			}
+
+			Platform plat = getItem(position);
+			ImageView ivLogo = (ImageView) convertView.findViewById(R.id.ivLogo);
+			Bitmap logo = getIcon(plat);
+			if (logo != null && !logo.isRecycled()) {
+				ivLogo.setImageBitmap(logo);
+			}
+			CheckedTextView ctvName = (CheckedTextView) convertView.findViewById(R.id.ctvName);
+			ctvName.setChecked(plat.isValid());
+			if (plat.isValid()) {
+				String userName = plat.getDb().get("nickname");
+				if (userName == null || userName.length() <= 0
+						|| "null".equals(userName)) {
+					// 如果平台已经授权却没有拿到帐号名称，则自动获取用户资料，以获取名称
+					userName = getName(plat);
+					plat.setPlatformActionListener(page);
+					plat.showUser(null);
+				}
+				ctvName.setText(userName);
+			}
+			else {
+				ctvName.setText(R.string.not_yet_authorized);
+			}
+			return convertView;
+		}
+
+		private Bitmap getIcon(Platform plat) {
+			if (plat == null) {
+				return null;
+			}
+
+			String name = plat.getName();
+			if (name == null) {
+				return null;
+			}
+
+			String resName = "logo_" + plat.getName();
+			int resId = cn.sharesdk.framework.utils.R.getResId(R.drawable.class, resName);
+			return BitmapFactory.decodeResource(page.menu.getResources(), resId);
+		}
+
+		private String getName(Platform plat) {
+			if (plat == null) {
+				return "";
+			}
+
+			String name = plat.getName();
+			if (name == null) {
+				return "";
+			}
+
+			int resId = cn.sharesdk.framework.utils.R.getStringRes(page.menu.getContext(), plat.getName());
+			return page.menu.getContext().getString(resId);
+		}
+
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			Platform plat = getItem(position);
+			CheckedTextView ctvName = (CheckedTextView) view.findViewById(R.id.ctvName);
+			if (plat == null) {
+				ctvName.setChecked(false);
+				ctvName.setText(R.string.not_yet_authorized);
+				return;
+			}
+
+			if (plat.isValid()) {
+				plat.removeAccount();
+				ctvName.setChecked(false);
+				ctvName.setText(R.string.not_yet_authorized);
+				return;
+			}
+
+			plat.setPlatformActionListener(page);
+			plat.showUser(null);
+		}
+
+	}
+
+}
